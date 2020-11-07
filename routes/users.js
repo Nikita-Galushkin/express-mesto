5be41c0fb8a13ff9ab0d90b63536b5210e7eb679
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-11-07 16:12:59 +0300

    remove

--- routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { getUsers, getUser } = require('../controllers/users');
-
-router.get('/users', getUsers);
-router.get('/users/:id', getUser);
-
-module.exports = router;
