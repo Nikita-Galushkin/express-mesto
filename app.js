bfd85408eb296a9f8abfe62649542d4c40e612ec
Nikita Galushkin <gavrik_nikitka@mail.ru>
2020-11-08 00:31:56 +0300

    fix linter errors

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const app = express();
 const path = require('path');
-const cardsRouter = require(path.join(__dirname, './routes/cards'));
-const usersRouter = require(path.join(__dirname, './routes/users'));
+const cardsRouter = require(path.join(__dirname, './routes/cards')); // eslint-disable-line import/no-dynamic-require
+const usersRouter = require(path.join(__dirname, './routes/users')); // eslint-disable-line import/no-dynamic-require
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 app.use('*', (req, res) => {
-  res.status(404).send({message: 'Запрашиваемый ресурс не найден'});
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`App listening on port ${PORT}`);
 });
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,11 @@
-const readFile = require('../utils/read-file');
 const path = require('path');
+const readFile = require('../utils/read-file');
 const pathToDataCards = path.join(__dirname, '..', 'data', 'cards.json');
 
 module.exports.getCards = (req, res) => {
   readFile(pathToDataCards)
     .then((data) => res.send(data))
-    .catch((err) => {
-      res.status(404).send({ message: "Нет такого файла" })
+    .catch(() => {
+      res.status(500).send({ message: 'Нет такого файла' });
     });
 };
